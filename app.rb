96edbdcf73995556f7487a6c24f5bd4490388a3f
sthai1721 <sophia.thai@rutgers.edu>
2018-08-14 18:56:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,27 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
 end
+
+class App < Sinatra::Base
+  get '/name' do
+    "My name is Soph"
+  end
+end
+
+class App < Sinatra::Base
+  get '/hometown' do
+    "My hometown is NJ"
+  end
+end
+
+class App < Sinatra::Base
+  get '/favorite-song' do
+    "My favorite song is Adulthood by Jukebox the Ghost"
+  end
+end
+
+
